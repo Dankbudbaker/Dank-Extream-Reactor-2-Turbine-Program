a9e15c8467cc3d3e3a8a8c27d2c07ec73ffeed29
Dankbudbaker <67717375+Dankbudbaker@users.noreply.github.com>
2021-01-20 14:07:31 -0700

    Update installer.lua

--- dankturbinecontrol_1/src/install/installer.lua
+++ dankturbinecontrol_1/src/install/installer.lua
@@ -39,7 +39,7 @@ else
 end
 
 --Url for file downloads
-local relUrl = "https://raw.githubusercontent.com/IllgiLP/Reactor-and-Turbine-control-program/"..branch.."/turbineControl_v2/src/"
+local relUrl = "https://raw.githubusercontent.com/Dankbudbaker/Dank-Extream-Reactor-2-Turbine-Program/"..branch.."/dankturbinecontrol_1/src/"
 
 
 --===== Functions =====
