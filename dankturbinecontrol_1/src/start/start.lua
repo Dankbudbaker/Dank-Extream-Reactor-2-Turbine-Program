ce01bb6fd8409d5f976813f89836e861ad6f251f
Dankbudbaker <67717375+Dankbudbaker@users.noreply.github.com>
2021-01-21 01:59:11 -0700

    Update start.lua

--- dankturbinecontrol_1/src/start/start.lua
+++ dankturbinecontrol_1/src/start/start.lua
@@ -22,7 +22,7 @@ _G.turbineOnOff = ""
 --Peripherals
 _G.mon = "" --Monitor
 _G.r = "" --Reactor
-_G.v = peripheral.wrap("bottom") --powah Energy Storage
+_G.v = peripheral.wrap("rftoolspower:cell3_0") --powah Energy Storage
 _G.t = {} --Turbines
 --Total count of all turbines
 _G.amountTurbines = 0
